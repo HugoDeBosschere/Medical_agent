c13ddbbb7134d48c01fe4de5ed8c630a94d81f37
Hugo <hugo.debosschere-work@proton.me>
2026-02-28 16:54:59 +0100

    added the evaluate response function

--- src/onco_veto/Rar.py
+++ src/onco_veto/Rar.py
@@ -3,7 +3,7 @@ from typing import List
 import pandas as pd
 from ollama import chat
 from deepeval import evaluate
-from deepeval.test_case import LLMTestCase
+from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 from deepeval.metrics import AnswerRelevancyMetric, GEval
 from deepeval import evaluate
 from deepeval.models import OllamaModel
@@ -43,7 +43,7 @@ def call_ollama_generate(prompt: str, model: str = MODEL) -> str:
     except requests.exceptions.RequestException as e:
         raise RuntimeError(f"Erreur lors de l'appel à Ollama: {e}")
 
-def evaluate_response(test_case: LLMTestCase, threshold: float, judge: str) -> bool:
+def evaluate_response(test_case: LLMTestCase, threshold: float, judge: OllamaModel) -> bool:
     """
     Returns True if the summary is good, False if it isn't.
     Acts as a first guardrail against LLM hallucinations.
@@ -53,11 +53,15 @@ def evaluate_response(test_case: LLMTestCase, threshold: float, judge: str) -> b
         model=judge, # You must pass your Ollama model string or wrapper here
         threshold=threshold, # Inject your custom threshold
         evaluation_steps=[
-            "Check whether the facts in 'actual output' contradicts any facts in 'expected output'",
-            "You should also heavily penalize omission of detail",
-            "Vague language, or contradicting OPINIONS, are OK"
+
+            "Check if only informations about the LUNGS are present in the output file",
+            "Check if the informations about the LUNGS that are in the output file are also in the input file",
+            "Check if there are informations about the LUNGS that are in the input file but are not in the output file ",
+            "Language should be precise and technical",
+            "The output text should be synthetic",
+            "Penalize texts that are too long"
         ],
-        evaluation_params=[test_case.INPUT, test_case.ACTUAL_OUTPUT]
+        evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT]
     )
 
     # 1. Execute the evaluation synchronously
@@ -73,11 +77,24 @@ def evaluate_response(test_case: LLMTestCase, threshold: float, judge: str) -> b
 
 # Exemple d'utilisation
 if __name__ == "__main__":
-    prompt = "Qu'est-ce que l'intelligence artificielle ?"
-    reponse = call_ollama_generate(prompt)
+    threshold = 0.5 
+    judge = OllamaModel(model="qwen3:0.6b")
+    document_no_tumeur = "Absence de comparatif disponible. Absence d'anomalie pleuroparenchymateuse pulmonaire. Absence d'épanchement pleural. Absence de pneumothorax. Silhouette cardiomédiastinale sans particularité. Structures osseuses et parties molles sans particularité."
+    document_tumeur = document_no_tumeur = "Absence de comparatif disponible. Absence d'anomalie pleuroparenchymateuse pulmonaire. Absence d'épanchement pleural. Absence de pneumothorax. Tumeur dans le poumon. Silhouette cardiomédiastinale sans particularité. Structures osseuses et parties molles sans particularité."
+    prompt = "Summarize the document and keep only the informations concerning the lungs" + document_tumeur
+    response: str = call_ollama_generate(prompt)
+    print(response)
+
+    test_case = LLMTestCase(
+        input = prompt,
+        actual_output = response
+    )
+
+    print(evaluate_response(test_case, threshold, judge))
+
     #print(f"Prompt: {prompt}")
     #print(f"Réponse: {reponse}")
-
+    """
     rar_augment = "\nRephrase and expand the question, and respond."
     promptRar = prompt + rar_augment
     reponse_Rar = call_ollama_generate(promptRar)
@@ -85,11 +102,11 @@ if __name__ == "__main__":
     #print(f"Réponse: {reponse_Rar}")
 
     test = LLMTestCase(input=prompt, actual_output=reponse)
-    judge = OllamaModel(model="qwen3:0.6b")
+    
     relevancy_metric = AnswerRelevancyMetric(threshold=0.5, model=judge)
     print(f"\n--- Test ---")
 
     relevancy_metric.measure(test)
     print(f"[AnswerRelevancy] Score : {relevancy_metric.score:.2f}")
     print(f"[AnswerRelevancy] Raison : {relevancy_metric.reason}")
-
+    """
